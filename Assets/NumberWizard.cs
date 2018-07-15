a76cbf19d6a8ae2fb12a206db9c32391530d538c
Tudor Enea <tudor_e@hotmail.com>
2018-07-15 14:04:36 +0200

    Final Version of "Number Wizard"!

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -2,43 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NumberWizard : MonoBehaviour {
+public class NumberWizard : MonoBehaviour
+{
 
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
 
     // Use this for initialization
-    void Start () {
-        
+    void Start()
+    {
+        StartGame();
+    }
 
-        Debug.Log("Hello there, welcome to Number Wizard");
-        Debug.Log("Think of a number, and keep it to yourself");
-        Debug.Log("The lowest number you can choose is: " + min);
-        Debug.Log("The highest number you can choose is: " + max);
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Hello There, Welcome to Number Wizard...");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
         Debug.Log("Tell me if your number is higher or lower than: " + guess);
-        Debug.Log("Push Up = Higher , Push Down = Lower , Push Enter = Correct");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
         max += 1;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("I'm a freaking genius!");
+            Debug.Log("I am a genius!");
+            StartGame();
         }
+    }
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Is it higher or lower than: " + guess);
     }
 }
